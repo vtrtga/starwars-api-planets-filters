f7c9543802b156e1c01e03a52f980ca01e75014b
vtrtga <vitor.ypvalim@gmail.com>
2022-07-12 17:16:18 -0300

    req1 e 2

--- src/components/Table.js
+++ src/components/Table.js
@@ -2,22 +2,51 @@ import React, { useContext } from 'react';
 import Context from '../context/Context';
 
 export default function Table() {
-  const { planets, isLoading } = useContext(Context);
-  console.log(planets);
+  const { filteredPlanets, isLoading, onChangeInput } = useContext(Context);
+  const headers = ['Name', 'Rotation Period', 'Orbital Period', 'Diameter', 'Climate',
+    'Gravity', 'Terrain', 'Surface Water', 'Population', 'Films', 'Created', 'Edited',
+    'URL'];
   return (
     <div>
+      <input
+        onChange={ onChangeInput }
+        data-testid="name-filter"
+        placeholder="Filtro"
+      />
       {
         isLoading ? (<p>Loading...</p>)
 
           : (
             <table>
-              {
-                planets.map((p) => (
-                  <tr key={ p.name }>
-                    <td>{p.name}</td>
-                  </tr>
-                ))
-              }
+              <thead>
+                <tr>
+
+                  {
+                    headers.map((h) => (<th key={ h }>{ h }</th>))
+                  }
+                </tr>
+              </thead>
+              <tbody>
+                {
+                  filteredPlanets.map((planet) => (
+                    <tr key={ planet.url }>
+                      <td>{planet.name}</td>
+                      <td>{planet.rotation_period}</td>
+                      <td>{planet.orbital_period}</td>
+                      <td>{planet.diameter}</td>
+                      <td>{planet.climate}</td>
+                      <td>{planet.gravity}</td>
+                      <td>{planet.terrain}</td>
+                      <td>{planet.surface_water}</td>
+                      <td>{planet.population}</td>
+                      <td>{planet.films.map((film) => film)}</td>
+                      <td>{planet.created}</td>
+                      <td>{planet.edited}</td>
+                      <td>{planet.url}</td>
+                    </tr>
+                  ))
+                }
+              </tbody>
             </table>
           )
       }
--- src/context/Provider.js
+++ src/context/Provider.js
@@ -5,19 +5,32 @@ import Context from './Context';
 export default function Provider({ children }) {
   const [isLoading, setLoading] = useState(true);
   const [planets, setPlanets] = useState([]);
+  const [filteredPlanets, setFilter] = useState([]);
 
-  //   console.log(planets);
+  const fetchApi = async () => {
+    const url = 'https://swapi-trybe.herokuapp.com/api/planets/';
+    const res = await fetch(url);
+    const resJson = await res.json();
+    const { results } = resJson;
+    console.log(results);
+    setPlanets(results);
+    setLoading(false);
+  };
   useEffect(() => {
-    fetch('https://swapi-trybe.herokuapp.com/api/planets/')
-      .then((res) => res.json())
-      .then((data) => {
-        setPlanets(data);
-      });
-    setLoading({ isLoading: false });
+    fetchApi();
   }, []);
 
+  const onChangeInput = ({ target }) => {
+    const { value } = target;
+    const planetsFilter = planets.filter(({ name }) => name.includes(value.toUpperCase())
+     || name.includes(value.toLowerCase()));
+    console.log({ planetsFilter, planets, value });
+    setFilter(planetsFilter);
+  };
   const context = {
-    ...planets,
+    filteredPlanets,
+    onChangeInput,
+    planets,
     isLoading,
   };
   return (
